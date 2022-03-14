c8ee51cfad84ca0f7cd45f8be9e84f1c4e81c4de
ProchyMicrochip <50979154+ProchyMicrochip@users.noreply.github.com>
2022-03-14 11:09:28 +0100

    BouncingButton

---
+++ 2. semestr - C#/Programovani/Programovani.VisualMovement/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Programovani.VisualMovement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.timer1 = new System.Timers.Timer();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize) (this.timer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20D;
+            this.timer1.SynchronizingObject = this;
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Elapsed);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button1.Location = new System.Drawing.Point(315, 196);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize) (this.timer1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button button1;
+        private System.Timers.Timer timer1;
+
+        #endregion
+    }
+}
---
+++ 2. semestr - C#/Programovani/Programovani.VisualMovement/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Drawing;
+using System.Timers;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace Programovani.VisualMovement
+{
+    public partial class Form1 : Form
+    {
+        struct MovementVector
+        {
+            public double Right;
+            public double Down;
+
+            public MovementVector(double i, double i1)
+            {
+                Right = i;
+                Down = i1;
+            }
+        }
+
+        private MovementVector _movementVector;
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            button1.Location = new Point((Size.Width - button1.Width) / 2, (Size.Height - button1.Height) / 2);
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            var mouse = (MouseEventArgs) e;
+            var angle = Math.Atan2(mouse.X - button1.Location.X- button1.Width/2, mouse.Y - button1.Location.Y- button1.Height/2);
+            _movementVector = new MovementVector(Math.Sin(angle), Math.Cos(angle));
+            timer1.Enabled = !timer1.Enabled;
+            if(!timer1.Enabled) button1.Location = new Point((Size.Width - button1.Width) / 2, (Size.Height - button1.Height) / 2);
+        }
+
+        private void CheckBoundingBox()
+        {
+            if (button1.Location.X <= 0 || button1.Location.X + button1.Width >= Size.Width)
+                _movementVector = new MovementVector(-_movementVector.Right, _movementVector.Down);
+            if (button1.Location.Y <= 0 || button1.Location.Y + button1.Height*2 +20 >= Size.Height)
+                _movementVector = new MovementVector(_movementVector.Right, -_movementVector.Down);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            button1.Location = new Point((Size.Width - button1.Width) / 2, (Size.Height - button1.Height) / 2);
+        }
+
+        private void timer1_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            CheckBoundingBox();
+            button1.Location = new Point((int) (button1.Location.X + _movementVector.Right * 10),
+                (int) (button1.Location.Y + _movementVector.Down * 10));
+        }
+    }
+}
